5cb5162a591d954213a51e9439c443d881f235a9
Radoslav Olekšák <oleksakradoslav@gmail.com>
2018-07-05 12:53:10 +0200

    pretty body json and xml

--- client/src/utils/ProxyUtil.js
+++ client/src/utils/ProxyUtil.js
@@ -1,3 +1,8 @@
+import xmlBeautify from 'xml-beautifier'
+import JSONFormatter from 'json-fmt'
+
+const fmt = new JSONFormatter(JSONFormatter.PRETTY);
+
 export default class ProxyUtil {
 
     static getHeadersString(headersObj) {
@@ -11,12 +16,41 @@ export default class ProxyUtil {
         return out
     }
 
+    static getContentType(headersObj) {
+        let contentType = headersObj['content-type'] || headersObj['Content-Type'] || undefined
+
+        if (typeof contentType !== 'undefined') {
+            return contentType.split(";")[0]
+        } else {
+            return null
+        }
+    }
+
+    static beautifyBody(headersObj, content) {
+        let contentType = ProxyUtil.getContentType(headersObj)
+
+        if (contentType !== null) {
+            switch (contentType) {
+                case "text/xml": 
+                    return xmlBeautify(content)
+                case "application/json":
+                    fmt.reset()
+                    fmt.append(content)
+                    return fmt.flush()
+                default:
+                    return content
+            }
+        } else {
+            return content;
+        }
+    }
+
     static rawRequest(request) {
-        return `${request.method} ${request.url} HTTP/${request.httpVersion}\n${ProxyUtil.getHeadersString(request.headers)}\n${request.body}`  
+        return `${request.method} ${request.url} HTTP/${request.httpVersion}\n${ProxyUtil.getHeadersString(request.headers)}${typeof request.body !== "undefined" ? `\n${ProxyUtil.beautifyBody(request.headers, request.body)}` : ``}`  
     }
 
     static rawResponse(response) {
-        return `HTTP/${response.httpVersion} ${response.statusCode} OK\n${ProxyUtil.getHeadersString(response.headers)}${typeof response.body !== "undefined" ? `\n${response.body}` : ``}`
+        return `HTTP/${response.httpVersion} ${response.statusCode} OK\n${ProxyUtil.getHeadersString(response.headers)}${typeof response.body !== "undefined" ? `\n${ProxyUtil.beautifyBody(response.headers, response.body)}` : ``}`
     }
 
     static filter = (needle, requests) => {
